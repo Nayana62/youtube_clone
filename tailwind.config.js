ff43f98a466f44d8e8c716381766cd7dd43cc62d
N M Nayana <nmnayana62@gmail.com>
2024-01-10 20:56:18 +0530

    added: breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: "550px",
+
+      md: "768px",
+
+      lg: "1300px",
+
+      xl: "1200px",
+
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
